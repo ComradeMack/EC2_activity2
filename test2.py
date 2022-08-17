be51b0d6e0e8d412f57d6b6dfa7c52533997f455
EC2 Default User <ec2-user@ip-172-31-2-244.ap-southeast-1.compute.internal>
2022-08-17 15:22:30 +0000

    second commit

---
+++ test2.py
@@ -0,0 +1,14 @@
+import time
+import pickle
+a = []
+for i in range( 50000 ):
+    q = time.time_ns()
+    a.append( q )
+
+print( a[0], a[1] )
+with open( './timestamp2.txt', 'w' ) as f:
+    idx = 0
+    for time in a:
+        f.write( str( idx ) + ', ' + str( ( time - a[0] ) * 1e-9 ) )
+        f.write( '\n' )
+        idx += 1
